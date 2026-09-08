a01f7c9bbbc5f030387860eb7079a8c73ab9f2e1
Andre <dremartinss07@gmail.com>
2026-09-08 00:00:32 +0000

    Done!

--- numberguesser.c
+++ numberguesser.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include <time.h>
 #define TRUE -1
 #define FALSE 0
@@ -12,31 +13,43 @@ int main(){
 	correct = rand();
 	correct = (correct % 100) + 1;
 	// debug
-	printf("Random #: ", "%d", correct);
+	//printf("Random #: %d \n", correct);
 
 	char name[20];
 	printf("Hi! What is your name? ");
 	scanf("%s", name);
-	printf("Nice to meet you ", "%s", name,". Let's play a game");
-	
+	printf("Nice to meet you %s. Let's play a game \n", name);
+
 	while (keepGoing){
 		turnCount++;
-		printf("Turn %d", turnCount, ") Please guess a number: ");
-		scanf("%d", guess);
-		if (guess = correct){
+		printf("Turn %d", turnCount);
+		printf(") Please guess a number: \n");
+		scanf("%d", &guess);
+		
+		if (guess < correct){
+			printf("too low \n");
+		}
+		if(guess > correct){
+			printf("too high \n");
+		}
+
+		 
+
+		if (guess == correct){
 			keepGoing = FALSE;
-			printf("You got it! \n");
-			if (turnCount > 7){
-				printf("Amazing job!");
-				else if (turncount = 7){
-					printf("Eh.");
-					} // end else if
-		       		else {
-					printf("You could have done better.");
-					} // end else
-			} // end if (guess = correct)
+			printf("That's a bingo!");
+		 if (turnCount < 7){
+			printf(" Great job!");
+		} else if (turnCount > 7){
+			printf(" You could have done better...");
+		} else {
+			printf(" Eh.");
+		}
+		} // end if (end of game)
+	} // end while
+	
 
 
 
-	return 0;
+return 0;
 } // end main
